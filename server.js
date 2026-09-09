e9c6d86274e4769c6e8b3fa2e79e0b8598cad267
harumiya0110-lab <haru.miya.0110@gmail.com>
2026-09-09 22:58:23 +0900

    Make Render server binding explicit

--- server.js
+++ server.js
@@ -327,7 +327,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`チャットサーバーがポート ${PORT} で起動しました`);
-  console.log(`ブラウザで http://localhost:${PORT} にアクセスしてください`);
 });
